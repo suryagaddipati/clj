2eefe3095026c70d51bb0b73ab0a372ec29f79a8
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-06-21 10:06:14 -0500

    afa

--- frequency.clj
+++ frequency.clj
@@ -3,17 +3,23 @@
 
 
 (defn updateCount [m v in]
-  (let [prvCnt (get m v 0)
+  ; (println m v)
+  (let [
+        prvCnt (get m v 0)
         prvCntKey (format "count-%d" prvCnt) 
+
         newCnt (if in (inc prvCnt) (dec prvCnt))
         newCntKey (format "count-%d" newCnt) 
         numUpdatedM (assoc m v  newCnt)
-        removePrevCountM (assoc numUpdatedM prvCntKey (disj (get m prvCntKey #{} )v))
-        addNewCountM (assoc removePrevCountM newCntKey (conj (get m prvCntKey #{} )v))]
-   addNewCountM)) 
 
-(defn updateCounti [m v in]
-(if (and (not in) (not (contains? m v))) m (updateCount m v in)))
+        removePrevCountM (assoc numUpdatedM prvCntKey (disj (get m prvCntKey #{} )v))
+        addNewCountM (assoc removePrevCountM newCntKey (conj (get m newCntKey #{} )v))]
+    ; (println removePrevCountM  addNewCountM)
+    addNewCountM)) 
+
+(defn updateCounti [m v insert]
+  ; (println m v)
+ (if (and (not insert) (not (contains? m v))) m (updateCount m v insert)))
   
     
          
@@ -28,16 +34,21 @@
     (case k
       1 [cnt (updateCounti m v true)]
       2 [cnt (updateCounti m v false)]
-      3  [(conj cnt  (if (> (count (get m (format "count-%d" v)  #{}) ) 0)    1 0) )m]
-      xs)))
+      3  [(conj cnt  (if (> (count (get m (format "count-%d" v)  #{}) ) 0)    1 0) )m])))
+      
   
 
-; (defn freqQuery [queries](first(r/fold (r/monoid folder (constantly [[] {}])) queries)))
-(defn freqQuery [queries](r/fold (r/monoid folder (constantly [[] {}])) queries))
+(defn freqQuery [queries](first(r/fold (r/monoid folder (constantly [[] {}])) queries)))
+; (defn freqQuery [queries](r/fold (r/monoid folder (constantly [[] {}])) queries))
   
 
 (freqQuery [[ 3 4]
-            [ 2 1003]
+            [ 1 1003]
             [ 1 10000029]
-            [ 3 1]])
+            [ 2 1003]
+            [ 2 10000029]
+            [ 1 10000029]
+            [ 3 1]
+            [ 3 2]])
+            
   
