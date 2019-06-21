c0781ece8ca4b2836391a1fdf265dc5fd7f75639
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-06-20 20:22:44 -0500

    afas

---
+++ frequency.clj
@@ -0,0 +1,43 @@
+(ns frequency)
+(require '[clojure.core.reducers :as r])
+
+
+(defn updateCount [m v in]
+  (let [prvCnt (get m v 0)
+        prvCntKey (format "count-%d" prvCnt) 
+        newCnt (if in (inc prvCnt) (dec prvCnt))
+        newCntKey (format "count-%d" newCnt) 
+        numUpdatedM (assoc m v  newCnt)
+        removePrevCountM (assoc numUpdatedM prvCntKey (disj (get m prvCntKey #{} )v))
+        addNewCountM (assoc removePrevCountM newCntKey (conj (get m prvCntKey #{} )v))]
+   addNewCountM)) 
+
+(defn updateCounti [m v in]
+(if (and (not in) (not (contains? m v))) m (updateCount m v in)))
+  
+    
+         
+    
+                         
+(defn folder [xs x] 
+  (let [cnt (first xs)
+        m (second xs)
+        k (first x)
+        v (second x)]
+        
+    (case k
+      1 [cnt (updateCounti m v true)]
+      2 [cnt (updateCounti m v false)]
+      3  [(conj cnt  (if (> (count (get m (format "count-%d" v)  #{}) ) 0)    1 0) )m]
+      xs)))
+  
+
+; (defn freqQuery [queries](first(r/fold (r/monoid folder (constantly [[] {}])) queries)))
+(defn freqQuery [queries](r/fold (r/monoid folder (constantly [[] {}])) queries))
+  
+
+(freqQuery [[ 3 4]
+            [ 2 1003]
+            [ 1 10000029]
+            [ 3 1]])
+  
