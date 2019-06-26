b7f6408155ba50421a35c0423b6b9d87b707a9d2
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-06-26 17:23:06 -0500

    dafaf

--- src/fourClj.clj
+++ src/fourClj.clj
@@ -25,15 +25,30 @@
 ;                     (println (func 1)))))
                     
 
-(def x (fn [func]  (fn [ & args] 
-                     (reduce #(%1 %2) func args))))
+; (def x (fn [func]  (fn [ & args]) 
+;                    (reduce #(%1 %2) func args)))
 
-((x (fn [a]
-        (fn [b]
-          (fn [c]
-            (fn [d]
-              (+ a b c d))))))
- 1 2 3 4)
+; ((x (fn [a]
+;         (fn [b]
+;           (fn [c]
+;             (fn [d]
+;               (+ a b c d))))))
+;  1 2 3 4)
    
+
+(def x (fn m [ & args] 
+         (lazy-seq
+           (let [calc ((second args) (first args))
+                  restArgs (rest args)
+                  fRestArgs (first restArgs)
+                  restRestArgs (rest restArgs)
+                  rotatedArgs (conj (vec restRestArgs)  fRestArgs)]
+              (cons calc (apply m calc rotatedArgs))))))  
+             
+
+          
+
+; (take 3 ( x 3.14 int double))
+(take 5 (x 3 #(- % 3) #(+ 5 %)))
 
   
