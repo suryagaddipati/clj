ce6a8f1b4209ba30e1c3e0c6213b3ef5428d5d44
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-08-02 16:41:33 -0500

    42

--- src/leet.clj
+++ src/leet.clj
@@ -10,14 +10,42 @@
            (str/includes? x "/") (reduce #(quot (k %1) (k %2) )  (split x "/"))
            :else (Integer/parseInt x)))) "3 / 2 *3")
 
+(defn pp[x] (do (print x) x))
+(defn sum [xs] (reduce + xs))
 
-((fn k [xs n]
-   (let [ sum #(reduce + %)
-         sets (fn [x n] (take-while #(< (sum %) n) (map #(repeat % x)(range))))]
-     (if (or (= n 0) (empty? xs))
-             []
-             (reduce  #(let [ rest-n (- n (sum %2))
-                             rest-k  [] ;(k (rest xs) rest-n)
-                             all-sums (map (fn[xs](concat %2 xs) rest-k))] (concat %1 all-sums))
-                      []
-                      (sets (first xs)))))) [2,3,5] 8)
+(defn repeat-till[x n] (take-while #(<= (sum %) n) (map #(repeat % x)(range))))
+; 39
+((fn ki [xs n]
+   (if (or (= n 0) (empty? xs))
+     []
+     (let [ fst (first xs)
+            rst (rest xs)
+           f-sets (repeat-till fst n)]
+       (reduce  #(let [ sum-set (sum %2)
+                       rest-n (- n sum-set)
+                       rest-k  (ki rst rest-n)
+                       all-sums (map (fn[xs](concat %2 xs)) rest-k)]
+                   (concat %1  (if (= sum-set n) [ %2] all-sums)))
+                []  f-sets
+                )))
+   ) [2 3 6 7 ] 7)
+
+
+;40
+
+((fn ki [xs n]
+   (let [ all-sets 
+         (if (or (<= n 0) (empty? xs))
+           #{}
+           (let [ fst (first xs)
+                 rst (rest xs)
+                 f-sets [#{} #{fst}]]
+             (reduce  #(let [ sum-set (sum %2)
+                             rest-n (- n sum-set)
+                             rest-k  (ki rst rest-n)
+                             all-sums (map (fn[xs](concat %2 xs)) rest-k)]
+                         (clojure.set/union %1 (if (= sum-set n) #{ %2} all-sums) ))
+                      #{}  f-sets
+                      ))
+           )] (reduce conj #{} all-sets))
+   )(sort [10,1,2,7,6,1,5])  8)
