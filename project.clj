7afb46e9b42efd574a176683abeaed10dd7004dc
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-08-03 13:30:46 -0500

    dasf

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject surya/fourClj "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.500"]
 ]
   )
